b0c4c511d8c49e849d7a579d6622304d8f859516
blink <threerapon.r@trendvg3.com>
2020-08-22 23:52:21 +0700

    rm clound

--- src/components/Headers/IndexHeader.js
+++ src/components/Headers/IndexHeader.js
@@ -26,9 +26,6 @@ function IndexHeader() {
               <div className="title-brand">
                 <h1 className="presentation-title">Art <br/> of Hongtae</h1>
               </div>
-              {/* <h2 className="presentation-subtitle text-center">
-                Make your mark with a Free Bootstrap 4 (Reactstrap) UI Kit!
-              </h2> */}
             </Container>
           </div>
           <div
@@ -41,48 +38,6 @@ function IndexHeader() {
       </>
     )
   }else{
-
-  }
-  // if(isMobile){
-  //   return
-  //   (
-  //     <>
-  //     <div
-  //         className="page-header section-dark"
-  //         style={{
-  //           backgroundImage:
-  //             "url(" + require("assets/img/header/mobileWall.jpg") + ")",
-  //         }}
-  //       >
-  //         <div className="filter" />
-  //         <div className="content-center">
-  //           <Container>
-  //             <div className="title-brand">
-  //               <h1 className="presentation-title">Art of Hongtae</h1>
-  //               <div className="fog-low">
-  //                 <img alt="..." src={require("assets/img/fog-low.png")} />
-  //               </div>
-  //               <div className="fog-low right">
-  //                 <img alt="..." src={require("assets/img/fog-low.png")} />
-  //               </div>
-  //             </div>
-  //             {/* <h2 className="presentation-subtitle text-center">
-  //               Make your mark with a Free Bootstrap 4 (Reactstrap) UI Kit!
-  //             </h2> */}
-  //           </Container>
-  //         </div>
-  //         <div
-  //           className="moving-clouds"
-  //           style={{
-  //             backgroundImage: "url(" + require("assets/img/clouds.png") + ")",
-  //           }}
-  //         />
-          
-  //       </div>
-  //   </>
-  //   )
-    
-  // }else{
     return (
       <>
         <div
@@ -93,22 +48,6 @@ function IndexHeader() {
           }}
         >
           <div className="filter" />
-          <div className="content-center">
-            <Container>
-              <div className="title-brand">
-                {/* <h1 className="presentation-title">Paper Kit React</h1> */}
-                <div className="fog-low">
-                  <img alt="..." src={require("assets/img/fog-low.png")} />
-                </div>
-                <div className="fog-low right">
-                  <img alt="..." src={require("assets/img/fog-low.png")} />
-                </div>
-              </div>
-              {/* <h2 className="presentation-subtitle text-center">
-                Make your mark with a Free Bootstrap 4 (Reactstrap) UI Kit!
-              </h2> */}
-            </Container>
-          </div>
           <div
             className="moving-clouds"
             style={{
@@ -118,10 +57,9 @@ function IndexHeader() {
           
         </div>
       </>
-    );
+    )
   }
+}
 
-  
-// }
 
 export default IndexHeader;
