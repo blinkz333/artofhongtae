40aedc8aa599b003cbbe57784557d6fd5ac577f5
blink <threerapon.r@trendvg3.com>
2020-09-02 20:30:41 +0700

    fix url link

--- src/components/Navbars/IndexNavbar.js
+++ src/components/Navbars/IndexNavbar.js
@@ -54,7 +54,7 @@ function IndexNavbar() {
         <div className="navbar-translate">
           <NavbarBrand
             data-placement="bottom"
-            to="https://blinkz333.github.io/art_of_hongtae/#/"
+            href="https://blinkz333.github.io/art_of_hongtae/#/"
             title="Art-Of-Hongtae"
             tag={Link}
           >
