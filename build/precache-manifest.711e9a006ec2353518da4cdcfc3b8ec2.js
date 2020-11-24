0b66e2be3136d5b6ff8182d58aec90654c77d04e
blink <threerapon.r@trendvg3.com>
2020-11-24 15:26:44 +0700

    build proj

---
+++ build/precache-manifest.711e9a006ec2353518da4cdcfc3b8ec2.js
@@ -0,0 +1,338 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1bf84f4b41b44bc61920052835a4356",
+    "url": "/artofhongtae/index.html"
+  },
+  {
+    "revision": "31c3d5cd58378c3a665a",
+    "url": "/artofhongtae/static/css/2.69507427.chunk.css"
+  },
+  {
+    "revision": "0e4b5b6f0a3e73064cb6",
+    "url": "/artofhongtae/static/css/main.cdb4a98b.chunk.css"
+  },
+  {
+    "revision": "31c3d5cd58378c3a665a",
+    "url": "/artofhongtae/static/js/2.15dbf0cd.chunk.js"
+  },
+  {
+    "revision": "3554eada3eace865b725bba98ea53ec1",
+    "url": "/artofhongtae/static/js/2.15dbf0cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e4b5b6f0a3e73064cb6",
+    "url": "/artofhongtae/static/js/main.08f267a1.chunk.js"
+  },
+  {
+    "revision": "36f183307bd59975c82d",
+    "url": "/artofhongtae/static/js/runtime-main.2832e0f1.js"
+  },
+  {
+    "revision": "08130410df1676104f2cdc37a5b66d19",
+    "url": "/artofhongtae/static/media/1.08130410.jpg"
+  },
+  {
+    "revision": "17221b988d932ef523f41566a530cdf9",
+    "url": "/artofhongtae/static/media/1.17221b98.jpg"
+  },
+  {
+    "revision": "3b5b541bc74d2a531b9d2b3537093174",
+    "url": "/artofhongtae/static/media/1.3b5b541b.jpg"
+  },
+  {
+    "revision": "76bc10be47f87a2d141884b775510f73",
+    "url": "/artofhongtae/static/media/1.76bc10be.jpg"
+  },
+  {
+    "revision": "6d971f576c410e5762afe293d2fb473c",
+    "url": "/artofhongtae/static/media/10.6d971f57.jpg"
+  },
+  {
+    "revision": "93f56c0d4f37eeeb1ce177878987c719",
+    "url": "/artofhongtae/static/media/10.93f56c0d.jpg"
+  },
+  {
+    "revision": "c7055a03493010caaa7ef6ed9e60afc9",
+    "url": "/artofhongtae/static/media/10.c7055a03.jpg"
+  },
+  {
+    "revision": "732681f3586e13a201e43bf7ffa8062b",
+    "url": "/artofhongtae/static/media/11.732681f3.jpg"
+  },
+  {
+    "revision": "84250818c8bf159bb6b30b2db49465c7",
+    "url": "/artofhongtae/static/media/11.84250818.jpg"
+  },
+  {
+    "revision": "97d37dd4867aebaa7ed25949847f4a40",
+    "url": "/artofhongtae/static/media/11.97d37dd4.jpg"
+  },
+  {
+    "revision": "c947721fd53245de315e84fbb13a1f1a",
+    "url": "/artofhongtae/static/media/12.c947721f.jpg"
+  },
+  {
+    "revision": "e5c00b82a143d60fa8d5b9d9d4f33f95",
+    "url": "/artofhongtae/static/media/12.e5c00b82.jpg"
+  },
+  {
+    "revision": "1709073a4577a8408bcb5e5d09192b1b",
+    "url": "/artofhongtae/static/media/13.1709073a.jpg"
+  },
+  {
+    "revision": "260becbd5ebcb726820d38afeae0559e",
+    "url": "/artofhongtae/static/media/13.260becbd.jpg"
+  },
+  {
+    "revision": "cc5b0c74f57d35a5d3d2e55175563668",
+    "url": "/artofhongtae/static/media/14.cc5b0c74.jpg"
+  },
+  {
+    "revision": "1901c4625177c014fa47b7e242a6207c",
+    "url": "/artofhongtae/static/media/15.1901c462.jpg"
+  },
+  {
+    "revision": "a5a36ed449d514c6967ca0af0355fdf3",
+    "url": "/artofhongtae/static/media/16.a5a36ed4.jpg"
+  },
+  {
+    "revision": "38d99ebc79341c33e9f334b229cc3074",
+    "url": "/artofhongtae/static/media/17.38d99ebc.jpg"
+  },
+  {
+    "revision": "408d25c8f462ab63580af4231fb3f2a1",
+    "url": "/artofhongtae/static/media/18.408d25c8.jpg"
+  },
+  {
+    "revision": "18f8f603a67a38579b78e86fffee2b9a",
+    "url": "/artofhongtae/static/media/19.18f8f603.jpg"
+  },
+  {
+    "revision": "77d44588793b0ba42282417a6a586fc4",
+    "url": "/artofhongtae/static/media/2.77d44588.jpg"
+  },
+  {
+    "revision": "9fbeca00996680b3e44215c73202a016",
+    "url": "/artofhongtae/static/media/2.9fbeca00.jpg"
+  },
+  {
+    "revision": "aa52cc6214330bd4097b4973b070670a",
+    "url": "/artofhongtae/static/media/2.aa52cc62.jpg"
+  },
+  {
+    "revision": "d1a7a211c95e8a31421dddba3bf22691",
+    "url": "/artofhongtae/static/media/2.d1a7a211.jpg"
+  },
+  {
+    "revision": "9dd5c7f7ff37fee5fc03992762a29064",
+    "url": "/artofhongtae/static/media/20.9dd5c7f7.jpg"
+  },
+  {
+    "revision": "77d118b3f624ff134e00f7d0eeafdc2e",
+    "url": "/artofhongtae/static/media/21.77d118b3.jpg"
+  },
+  {
+    "revision": "4f87da560c2f9fecc5d48ab8b352f3f7",
+    "url": "/artofhongtae/static/media/22.4f87da56.jpg"
+  },
+  {
+    "revision": "df29cc28dd611e606fb04243a3314d55",
+    "url": "/artofhongtae/static/media/23.df29cc28.jpg"
+  },
+  {
+    "revision": "c2bc87affe608c41fada9531ecc330a6",
+    "url": "/artofhongtae/static/media/24.c2bc87af.jpg"
+  },
+  {
+    "revision": "30d369fab5db1d103cbcf45ca5dcaded",
+    "url": "/artofhongtae/static/media/25.30d369fa.jpg"
+  },
+  {
+    "revision": "002d9814f3cb177e31402e1f20c0da34",
+    "url": "/artofhongtae/static/media/26.002d9814.jpg"
+  },
+  {
+    "revision": "d28796846531badc2b4bbd211d2f8f91",
+    "url": "/artofhongtae/static/media/27.d2879684.jpg"
+  },
+  {
+    "revision": "cdeede56ad900bbf591fc16b55de64a9",
+    "url": "/artofhongtae/static/media/28.cdeede56.jpg"
+  },
+  {
+    "revision": "6484b2dadcf1db8877d392ceb9e52e94",
+    "url": "/artofhongtae/static/media/3.6484b2da.jpg"
+  },
+  {
+    "revision": "7e212743e4c8e499e7ec6d97ef432617",
+    "url": "/artofhongtae/static/media/3.7e212743.jpg"
+  },
+  {
+    "revision": "b6207de1c5a48b3c6f80d0cb9144217b",
+    "url": "/artofhongtae/static/media/3.b6207de1.jpg"
+  },
+  {
+    "revision": "c069e0f0bba481dacf05fd6cc11856b2",
+    "url": "/artofhongtae/static/media/3.c069e0f0.jpg"
+  },
+  {
+    "revision": "37c66863dabd942cb5fb60a9f303d2d1",
+    "url": "/artofhongtae/static/media/4.37c66863.jpg"
+  },
+  {
+    "revision": "70628e70688cf529376d9fc43826ba8f",
+    "url": "/artofhongtae/static/media/4.70628e70.jpg"
+  },
+  {
+    "revision": "aa7ad464e110c2b32955e657cd53350d",
+    "url": "/artofhongtae/static/media/4.aa7ad464.jpg"
+  },
+  {
+    "revision": "ba37bc6e9e744990bd8bb65b38cf282e",
+    "url": "/artofhongtae/static/media/4.ba37bc6e.jpg"
+  },
+  {
+    "revision": "1d15ffb18c35b243288c351be29c8402",
+    "url": "/artofhongtae/static/media/5.1d15ffb1.jpg"
+  },
+  {
+    "revision": "6719650dfbbfae863665b4d030545def",
+    "url": "/artofhongtae/static/media/5.6719650d.jpg"
+  },
+  {
+    "revision": "6f0d53d2cd87749b0c80cf4630f67a29",
+    "url": "/artofhongtae/static/media/5.6f0d53d2.jpg"
+  },
+  {
+    "revision": "7057a821c6a771dd58a8480580f61697",
+    "url": "/artofhongtae/static/media/5.7057a821.jpg"
+  },
+  {
+    "revision": "7769ecb4810a1fa43214985d9a98822c",
+    "url": "/artofhongtae/static/media/6.7769ecb4.jpg"
+  },
+  {
+    "revision": "f148f43333f9ca8cdfe197e3be4a79f8",
+    "url": "/artofhongtae/static/media/6.f148f433.jpg"
+  },
+  {
+    "revision": "ff94c431c227ea33dc42f3512cf88a1b",
+    "url": "/artofhongtae/static/media/6.ff94c431.jpg"
+  },
+  {
+    "revision": "3852f797ebd6bb6d1cc443986c63aaea",
+    "url": "/artofhongtae/static/media/7.3852f797.jpg"
+  },
+  {
+    "revision": "b9949f88fd52f5a0bb219382262abcbe",
+    "url": "/artofhongtae/static/media/7.b9949f88.jpg"
+  },
+  {
+    "revision": "bdd3d18fa692b95841ad1b375a3ca916",
+    "url": "/artofhongtae/static/media/7.bdd3d18f.jpg"
+  },
+  {
+    "revision": "265fb98eabab4eb1cc495ddeafa71f44",
+    "url": "/artofhongtae/static/media/8.265fb98e.jpg"
+  },
+  {
+    "revision": "6453c64c4828de0977a70638edd93b6a",
+    "url": "/artofhongtae/static/media/8.6453c64c.jpg"
+  },
+  {
+    "revision": "afe90116c95a74549ecb735f871a0ac6",
+    "url": "/artofhongtae/static/media/8.afe90116.jpg"
+  },
+  {
+    "revision": "20134eec8b02192424432a8542c47038",
+    "url": "/artofhongtae/static/media/9.20134eec.jpg"
+  },
+  {
+    "revision": "6eb65498a71922189f3ad7bbf576705d",
+    "url": "/artofhongtae/static/media/9.6eb65498.jpg"
+  },
+  {
+    "revision": "e82f6daadf9a0487e91b5f33b69ee405",
+    "url": "/artofhongtae/static/media/9.e82f6daa.jpg"
+  },
+  {
+    "revision": "bd0329396e707c47e34585c51931ed39",
+    "url": "/artofhongtae/static/media/DisneyEx1.bd032939.jpg"
+  },
+  {
+    "revision": "e5c64044d1aac755bd84dead81e9e15c",
+    "url": "/artofhongtae/static/media/DisneyEx2.e5c64044.jpg"
+  },
+  {
+    "revision": "fca97f3d79f6efb27d384f544dc0bd96",
+    "url": "/artofhongtae/static/media/DisneyEx3.fca97f3d.jpg"
+  },
+  {
+    "revision": "7b33d60fc8bdeaa66e001800a7d4fd49",
+    "url": "/artofhongtae/static/media/LongkaEx1.7b33d60f.jpg"
+  },
+  {
+    "revision": "05406b3b8bcd6dde45e32317a9550539",
+    "url": "/artofhongtae/static/media/LongkaEx2.05406b3b.jpg"
+  },
+  {
+    "revision": "2a4cf9e83d52f567b391f50149594a34",
+    "url": "/artofhongtae/static/media/LongkaEx3.2a4cf9e8.jpg"
+  },
+  {
+    "revision": "61c4e0007254eb55e4691291881a4bfb",
+    "url": "/artofhongtae/static/media/clouds.61c4e000.png"
+  },
+  {
+    "revision": "f1c708978b42cd0b8b7abd4c6748fd0b",
+    "url": "/artofhongtae/static/media/header4.f1c70897.jpg"
+  },
+  {
+    "revision": "9bb2ea978a0e436d32d689ee03549ee5",
+    "url": "/artofhongtae/static/media/mobileWall.9bb2ea97.jpg"
+  },
+  {
+    "revision": "48ac036da7c7923266266f63746fc5f1",
+    "url": "/artofhongtae/static/media/nucleo-icons.48ac036d.ttf"
+  },
+  {
+    "revision": "5c78b4cf03771fd2287325c44f0d3cd9",
+    "url": "/artofhongtae/static/media/nucleo-icons.5c78b4cf.woff2"
+  },
+  {
+    "revision": "6836b93bf1af0b571928d98458a68445",
+    "url": "/artofhongtae/static/media/nucleo-icons.6836b93b.woff"
+  },
+  {
+    "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
+    "url": "/artofhongtae/static/media/nucleo-icons.f32ed5ad.eot"
+  },
+  {
+    "revision": "ceb13af77e8337559d8a40c842ec7a42",
+    "url": "/artofhongtae/static/media/profile.ceb13af7.jpg"
+  },
+  {
+    "revision": "e01f23f1d7743f49b3b1ce651228c883",
+    "url": "/artofhongtae/static/media/ramakienEx1.e01f23f1.jpg"
+  },
+  {
+    "revision": "ba451abc5d7bae73263d484c29b88ef8",
+    "url": "/artofhongtae/static/media/ramakienEx2.ba451abc.jpg"
+  },
+  {
+    "revision": "83c892e84358e702e34f49ae1175ed0d",
+    "url": "/artofhongtae/static/media/ramakienEx3.83c892e8.jpg"
+  },
+  {
+    "revision": "998bb5b41b7d1026613ea22b36800acb",
+    "url": "/artofhongtae/static/media/thaiBookEx1.998bb5b4.jpg"
+  },
+  {
+    "revision": "7cb6c0aaa4c606805ad63a110fcc7ef8",
+    "url": "/artofhongtae/static/media/thaiBookEx2.7cb6c0aa.jpg"
+  },
+  {
+    "revision": "dd434fa5a6ee5a5bc73c2d721977a5ed",
+    "url": "/artofhongtae/static/media/thaiBookEx3.dd434fa5.jpg"
+  }
+]);
